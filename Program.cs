32f31f66db318981f1c8d126f8c40bd1a63d2c7f
kocurc <kordian.o@wp.pl>
2020-09-08 22:02:42 +0200

    Initial commit. Solution for: https://pl.spoj.com/problems/PRIME_T/

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Spoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
