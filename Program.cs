281622bad502d7780f86f399738e22897042c193
kocurc <kordian.o@wp.pl>
2020-09-08 22:47:49 +0200

    Solution for challenge.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace Spoj
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int howMany = int.Parse(Console.ReadLine());
+            int[] array = new int[howMany];
+
+            for (int i = 0; i < howMany; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < howMany; i++)
+            {
+                if (array[i] == 1)
+                {
+                    Console.WriteLine("NIE");
+
+                    continue;
+                }
+
+                bool isPrime = true;
+
+                for (int j = 2; j < array[i]; j++)
+                {
+                    if (array[i] % j == 0)
+                    {
+                        isPrime = false;
+                        Console.WriteLine("NIE");
+
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine("TAK");
+                }
+            }
         }
     }
 }
